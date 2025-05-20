874c43c07171b1cc7f5699d61d96e2a30e198e10
feelkdn <doxdoxdox@naver.com>
2025-05-20 22:30:46 +0900

    config 에러수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        extend: {
+            borderColor: {
+                border: 'hsl(var(--border))', // border-border 지원
+            },
+            backgroundColor: {
+                background: 'hsl(var(--background))',
+            },
+            textColor: {
+                foreground: 'hsl(var(--foreground))',
+            },
+            // 필요 시 추가로 다른 CSS 변수도 등록 가능
+        },
+    },
+    plugins: [],
 }
-
