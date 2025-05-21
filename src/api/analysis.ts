33cf087447865ccf6416d2c869f86a4174da2489
keekkim <keek199502@gmail.com>
2025-05-21 19:53:45 +0900

    add call topic api

--- src/api/analysis.ts
+++ src/api/analysis.ts
@@ -11,7 +11,16 @@ export const fetchEmotionAnalysis = async (fileName: string) => {
 };
 
 export const fetchMetrics = async (fileName: string) => {
-  const response = await fetch(DOMAIN + `/metrics/` + fileName);
+  const response = await fetch(DOMAIN + `/pattern/` + fileName);
+  if (!response.ok) {
+    throw new Error('Failed to fetch emotion analysis data');
+  }
+  return response.json();
+};
+
+export const fetchTopic = async (fileName: string) => {
+  //const response = await fetch(DOMAIN + `/topic/` + fileName);
+  const response = await fetch(DOMAIN + `/topic/`);
   if (!response.ok) {
     throw new Error('Failed to fetch emotion analysis data');
   }
